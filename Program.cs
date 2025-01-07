4ba0728ec69d468836928e759b74763a3d4221b8
Silvia-kc <kacakovasilvia@gmail.com>
2025-01-07 08:20:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TargetReflection;
+
+Spy spy = new Spy();
+Console.WriteLine(spy.StealFieldInfo("Target", "PublicInfo", "PrivateInfo"));
+
+Console.WriteLine(spy.AnalyzeAccessModifiers("Target"));
+
+Console.WriteLine(spy.RevealPrivateMethods("Target"));
+
+Console.WriteLine(spy.CollectGettersAndSetters("Target"));
---
+++ Target.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetReflection
+{
+    public class Target
+    {
+        public string PublicInfo = "PublicData";
+        private string PrivateInfo = "SecretData";
+
+        public int PublicId { get; set; }
+        private double PrivateBalance { get; set; }
+
+        public Target()
+        {
+            this.PublicId = 42;
+            this.PrivateBalance = 1000.50;
+        }
+
+        public string GetPublicInfo()
+        {
+            return this.PublicInfo;
+        }
+
+        private string GetPrivateInfo()
+        {
+            return this.PrivateInfo;
+        }
+
+        private void UpdateBalance(double amount)
+        {
+            this.PrivateBalance += amount;
+        }
+    }
+}
+
+
+
+
